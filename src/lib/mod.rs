45b5e73ac685aa0be51dbc762ba810fd11cd27a7
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-04-15 13:17:39 -0400

    publish all

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -5,12 +5,9 @@ mod gpio;
 mod memory;
 mod registers;
 
-pub use board::setup_board;
-pub use gpio::pins::{
-    Function, Pin, ReadablePin, ReadablePinOptions, ReadablePins, WritablePin, WritablePinOptions,
-    WritablePins,
-};
-pub use gpio::ports::{Port, PortOptions, UsablePort};
+pub use board::*;
+pub use gpio::pins::*;
+pub use gpio::ports::*;
 
 pub const H: bool = true;
 pub const L: bool = false;
